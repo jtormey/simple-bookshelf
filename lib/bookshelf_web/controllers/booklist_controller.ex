c65a8fa339c749d25fc609fa7484d06b42c33bd9
Justin Tormey <jrtormey@gmail.com>
2019-11-15 10:35:09 -0600

    Create basic bookshelf view

--- lib/bookshelf_web/controllers/booklist_controller.ex
+++ lib/bookshelf_web/controllers/booklist_controller.ex
@@ -1,14 +1,18 @@
 defmodule BookshelfWeb.BookListController do
   use BookshelfWeb, :controller
 
-  alias Bookshelf.Accounts
+  alias Bookshelf.{Accounts, AirtableApi}
 
   def index(conn, %{"username" => username}) do
     case Accounts.get_account_by(username: username) do
       nil ->
         render(conn, "not_found.html", username: username)
       account ->
-        render(conn, "index.html", account: account)
+        {:ok, books} = AirtableApi.get_records!("Books", account)
+
+        categories = Enum.group_by books, fn r -> get_in(r, ["fields", "Status"]) end
+
+        render conn, "index.html", account: account, categories: categories
     end
   end
 end
--- lib/bookshelf_web/views/book_list_view.ex
+++ lib/bookshelf_web/views/book_list_view.ex
@@ -1,3 +1,12 @@
 defmodule BookshelfWeb.BookListView do
   use BookshelfWeb, :view
+
+  def color_for_status(status) do
+    case status do
+      "Currently Reading" -> "#FFEAB6"
+      "Finished" -> "#C2F5E9"
+      "Not Started" -> "#EEEEEE"
+      "Set Aside" -> "#FFDCE5"
+    end
+  end
 end
