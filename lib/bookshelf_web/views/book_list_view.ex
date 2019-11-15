86b33b6e62e180f08c6892cfaf567e8e746d9888
Justin Tormey <jrtormey@gmail.com>
2019-11-15 11:03:30 -0600

    Add description with started and finished dates

--- lib/bookshelf_web/views/book_list_view.ex
+++ lib/bookshelf_web/views/book_list_view.ex
@@ -9,4 +9,24 @@ defmodule BookshelfWeb.BookListView do
       "Set Aside" -> "#FFDCE5"
     end
   end
+
+  def description_for_book("Currently Reading", %{"Date Started" => started_date}) do
+    "Started #{date_format(started_date)}"
+  end
+
+  def description_for_book("Finished", %{"Date Finished" => finished_date}) do
+    "Finished #{date_format(finished_date)}"
+  end
+
+  def description_for_book("Set Aside", %{"Date Started" => started_date}) do
+    "Started #{date_format(started_date)}"
+  end
+
+  def description_for_book(_status, _fields), do: ""
+
+  def date_format(date) do
+    date
+    |> Date.from_iso8601!()
+    |> Timex.format!("{Mshort} {D}")
+  end
 end
--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Bookshelf.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:httpoison, "~> 1.6"},
+      {:timex, "~> 3.6.1"},
       {:plug_cowboy, "~> 2.0"},
       {:basic_auth, "~> 2.2.3"}
     ]
