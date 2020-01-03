62d6a98dc4a8e84e5aa063c647bf4d2c657fdbb2
Justin Tormey <jrtormey@gmail.com>
2020-01-03 11:58:41 -0600

    Configure deliver to build assets during release

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Google Cloud
 
 Deploy: `gcloud app deploy --project solar-vertex-259118`
+
+## Delivery
+
+* Build a release: `mix edeliver build release`
+* Deploy a release: `mix edeliver deploy release to production`
+* Start a release: `mix edeliver start production`
+* Migrate database: `mix edeliver migrate production`
--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :bookshelf, BookshelfWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "simplebookshelf.com", port: 443],
+  url: [scheme: "https", host: "digitalocean.simplebookshelf.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   code_reloader: false
