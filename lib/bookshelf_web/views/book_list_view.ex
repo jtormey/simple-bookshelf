6bc69d9393079139e4d6f977a806cc74a989ee37
Justin Tormey <jrtormey@gmail.com>
2020-01-08 15:04:09 -0600

    Order books from most recent

--- lib/bookshelf_web/views/book_list_view.ex
+++ lib/bookshelf_web/views/book_list_view.ex
@@ -25,8 +25,33 @@ defmodule BookshelfWeb.BookListView do
   def description_for_book(_status, _fields), do: ""
 
   def date_format(date) do
-    date
-    |> Date.from_iso8601!()
-    |> Timex.format!("{Mshort} {D}")
+    date = Date.from_iso8601!(date)
+    if Date.year_of_era(date) == Date.year_of_era(Date.utc_today()) do
+      Timex.format!(date, "{Mshort} {D}")
+    else
+      Timex.format!(date, "{Mshort} {D}, {YYYY}")
+    end
+  end
+
+  def date_order_field("Currently Reading", %{"Date Started" => started_date}) do
+    Date.from_iso8601!(started_date)
+  end
+
+  def date_order_field("Finished", %{"Date Finished" => finished_date}) do
+    Date.from_iso8601!(finished_date)
+  end
+
+  def date_order_field("Set Aside", %{"Date Started" => started_date}) do
+    Date.from_iso8601!(started_date)
+  end
+
+  def order_section(books, "Not Started"), do: books
+
+  def order_section(books, status) do
+    Enum.sort_by(
+      books,
+      fn book -> date_order_field(status, book["fields"]) end,
+      fn date_1, date_2 -> Date.compare(date_1, date_2) != :lt end
+    )
   end
 end
